e863413cac861f180baedca85c1cbce1679e4755
hyunjun <rlfahs3025@kaist.ac.kr>
2024-05-14 00:42:59 +0900

    Implement mul/div/quo/rem

--- main.py
+++ main.py
@@ -23,10 +23,46 @@ class Calculator:
         print(f"Result of substraction is {float(a) - float(b)}!")
 
     @staticmethod
+    def mul(a: int | float, b: int | float):
+        print("Please first number : ")
+        a = input()
+        print("Please second number : ")
+        b = input()
+        print(f"Result of multiplication is {float(a) * float(b)}!")
+
+    @staticmethod
+    def div(a: int | float, b: int | float):
+        print("Please first number : ")
+        a = input()
+        print("Please second number : ")
+        b = input()
+        print(f"Result of substraction is {float(a) / float(b)}!")
+
+    @staticmethod
+    def quotient(a: int, b: int):
+        print("Please first number : ")
+        a = input()
+        print("Please second number : ")
+        b = input()
+        print(f"Result of quotient is {int(a) // int(b)}!")
+
+    @staticmethod
+    def remainder(a: int, b: int):
+        print("Please first number : ")
+        a = input()
+        print("Please second number : ")
+        b = input()
+        print(f"Result of remainder is {int(a) // int(b)}!")
+
+    @staticmethod
     def print_option():
         print("Choose Option")
         print("(1) ADDITION")
         print("(2) SUBSTRACTION")
+        print("(3) MULTIPLICATION")
+        print("(4) DIVISION")
+        print("(5) QUOTIENT")
+        print("(6) REMAINDER")
         print("(Q) Quit")
 
 
@@ -40,6 +76,14 @@ def main():
             Calculator.add()
         elif a == "2":
             Calculator.sub()
+        elif a == "3":
+            Calculator.mul()
+        elif a == "4":
+            Calculator.div()
+        elif a == "5":
+            Calculator.quotient()
+        elif a == "6":
+            Calculator.remainder()
         elif a == "q" or "Q":
             break
         else:
